c134dd10d7a385c58ee6e45a289c7f626239e579
Tresforce <57471573+Tresforce@users.noreply.github.com>
2022-12-06 15:07:11 -0800

    updated

--- src/tests/integration/routes/user.spec.ts
+++ src/tests/integration/routes/user.spec.ts
@@ -3,18 +3,7 @@
  * @group integration
  */
 
-import { Connection } from 'typeorm';
-import { pgConnection } from '../../setup';
-
 describe('user route', () => {
-  let connection: Connection;
-  beforeAll(async () => {
-    connection = await pgConnection();
-  });
-  afterAll(async () => {
-    await connection.close();
-  });
-
   test.todo('should create user');
   test.todo('should update user');
   test.todo('should disable user');
